a42ba847b99818cbe12406ca564811a238fb0cb3
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-02-22 10:09:09 +0530

    add

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Wait for all images to load before displaying them
+window.addEventListener("load", function() {
+    const images = document.querySelectorAll(".photo-container img");
+    let loadedImages = 0;
+    
+    images.forEach(function(image) {
+      image.addEventListener("load", function() {
+        loadedImages++;
+        
+        if (loadedImages === images.length) {
+          document.querySelector(".photo-container").style.visibility = "visible";
+        }
+      });
+    });
+  });
+  
